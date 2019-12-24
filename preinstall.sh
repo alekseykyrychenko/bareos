ec7f976cdd62924b0952314f93ce031bb8be14b8
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-24 14:22:27 +0200

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,4 +1,10 @@
 echo "Generating mysql config file..."
-sed -i "s/{DATABASE}/${DATABASE}/" /etc/my.cnf.d/client.cnf
-sed -i "s/{USER}/${USER}/" /etc/my.cnf.d/client.cnf
-sed -i "s/{PASSWORD}/${PASSWORD}/" /etc/my.cnf.d/client.cnf
+cat mysql_client.cnf > /etc/my.cnf.d/client.cnf
+cat mysql_MyCatalog.conf > /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+for FILE in '/etc/my.cnf.d/client.cnf' '/etc/bareos/bareos-dir.d/catalog/MyCatalog.conf'
+do
+ echo 'Set ENV for: '${FILE}
+ sed -i "s/{DATABASE}/${DATABASE}/" ${FILE}
+ sed -i "s/{USER}/${USER}/" ${FILE}
+ sed -i "s/{PASSWORD}/${PASSWORD}/" ${FILE} 
+done
