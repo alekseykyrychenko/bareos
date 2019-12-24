fa7a25cbd22bc7e90be1d7830b574b9ffeff47a1
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-24 16:37:29 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,6 +1,5 @@
 echo "Starting Servis..."
 cd /var/lib/bareos
-/usr/lib/bareos/scripts/make_bareos_tables
 
 /usr/sbin/bareos-dir -t -f && /usr/sbin/bareos-dir && echo 'bareos-dir started' &&\
 /usr/sbin/bareos-sd && /usr/sbin/bareos-fd && /usr/sbin/httpd &&\
