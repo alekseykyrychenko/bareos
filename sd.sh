1ca284400355e0959be130f756d6695a96239254
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-25 20:12:42 +0200

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+  while :
+  do
+     echo '-'
+     if [[ -f /etc/bareos/bconsole.conf ]]; then break; fi
+     sleep 5
+  done
+  chown bareos.bareos /var/lib/bareos/storage
+  chmod 775 /var/lib/bareos/storage 
