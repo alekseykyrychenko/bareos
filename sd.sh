925355a2b2829a76dfeaf88a9ef23713c5d186db
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-25 20:15:06 +0200

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-  while :
+  
+while :
   do
      echo '-'
      if [[ -f /etc/bareos/bconsole.conf ]]; then break; fi
      sleep 5
   done
-  chown bareos.bareos /var/lib/bareos/storage
-  chmod 775 /var/lib/bareos/storage 
+
+chown bareos.bareos /var/lib/bareos/storage
+chmod 775 /var/lib/bareos/storage 
+echo "Starting Servis..."
+cd /var/lib/bareos
+/usr/sbin/bareos-sd
+
+while :
+do
+   sleep 1
+done
+echo "Exit"
