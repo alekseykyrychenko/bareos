267b89c519d60012647fa5d4b6ab720b45f8bfef
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-25 17:09:36 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -3,6 +3,12 @@ cd /var/lib/bareos
 
 /usr/sbin/bareos-dir -t -f && /usr/sbin/bareos-dir && echo 'bareos-dir started' &&\
 /usr/sbin/bareos-sd && /usr/sbin/bareos-fd && /usr/sbin/httpd
+sleep 2
+echo 'Create user: '${ADMIN_USER}
+bconsole << EOF
+configure add console name=${ADMIN_USER} password=${ADMIN_PASS} profile=webui-admin tlsenable=false
+EOF
+
 while :
 do
    sleep 1
