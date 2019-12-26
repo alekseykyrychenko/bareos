c7e4ea32074695b17fb402d908a0d2da647421ae
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-26 12:09:25 +0200

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -1,3 +1,8 @@
 echo 'Start sshd'
+
+mkdir /root/.ssh
+echo 'ssh-rsa '"${PUB_KEY}"' root@server.loc' > /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/authorized_keys
+
 /usr/sbin/sshd -D
 echo 'Stop'
