1fe202451e5881daee7ec30dfa8d8e638f342c99
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-26 10:21:01 +0200

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,7 +1,6 @@
 echo "Starting Servis..."
 
-echo 'ServerLimit 3
-StartServers 1
+echo 'StartServers 2
 MinSpareServers 2' >> /etc/httpd/conf.modules.d/00-mpm.conf
 
 sed -i 's/.*diraddress = .*/diraddress = "'${HOST}'"/' /etc/bareos-webui/directors.ini
