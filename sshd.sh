28ec0c75bad4cdffc5a92990f780de4291c3f431
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-26 16:00:49 +0200

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -3,6 +3,6 @@ echo 'Start sshd'
 mkdir /root/.ssh
 echo 'ssh-rsa '"${PUB_KEY}"' root@server.loc' > /root/.ssh/authorized_keys
 chmod 400 /root/.ssh/authorized_keys
-
+sed -i 's/.*GatewayPorts.*/GatewayPorts yes/' /etc/ssh/sshd_config
 /usr/sbin/sshd -D
 echo 'Stop'
