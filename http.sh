87ff9a81ac55ad1495e5359e778530861613cd69
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-26 09:22:55 +0200

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,12 +1,6 @@
 echo "Starting Servis..."
 
-http
-sleep 2
-echo 'Create user: '${ADMIN_USER}
-bconsole << EOF
-configure add console name=${ADMIN_USER} password=${ADMIN_PASS} profile=webui-admin tlsenable=false
-reload
-EOF
+/usr/sbin/httpd && echo OK
 
 while :
 do
