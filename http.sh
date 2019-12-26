e2ce93e51dd0dc11bd19d0ba86234e687e3c67c9
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2019-12-26 09:54:28 +0200

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,5 +1,12 @@
 echo "Starting Servis..."
 
+echo 'ServerLimit 1
+> StartServers 1
+> MinSpareServers 1
+> MaxConnectionsPerChild 1' >> /etc/httpd/conf.modules.d/00-mpm.conf
+
+diraddress = "localhost"
+
 /usr/sbin/httpd && echo OK
 
 while :
